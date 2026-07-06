34c854d3631b34fe07d8509f9b8451d08d8d4b5d
kushcmak <kushcmak@fel.cvut.cz>
2026-07-06 18:02:40 +0200

    chore: final adjustments to publish the mod

--- src/main/java/com/reign/itemlore/access/AnvilLoreScreenBridge.java
+++
@@ -1,9 +0,0 @@
-package com.reign.itemlore.access;
-
-import java.util.List;
-
-public interface AnvilLoreScreenBridge {
-	void itemLore$acceptServerLoreState(int containerId, int sessionId, String rawLoreMarkup, String rawNameMarkup);
-
-	List<RecipeViewerArea> itemLore$getRecipeViewerExclusionAreas();
-}
--- src/main/java/com/reign/itemlore/access/RecipeViewerArea.java
+++
@@ -1,4 +0,0 @@
-package com.reign.itemlore.access;
-
-public record RecipeViewerArea(int x, int y, int width, int height) {
-}
